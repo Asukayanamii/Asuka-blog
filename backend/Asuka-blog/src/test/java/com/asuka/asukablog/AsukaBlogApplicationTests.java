56d72f9547f315e406871bf16f18919698cf8a74
Asukayanami <3184311912@qq.com>
2026-05-12 18:23:33 +0800

    vue+springboot初始代码

---
+++ backend/Asuka-blog/src/test/java/com/asuka/asukablog/AsukaBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asuka.asukablog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsukaBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
