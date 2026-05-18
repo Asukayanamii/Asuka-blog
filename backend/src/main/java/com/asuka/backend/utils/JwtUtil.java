465a29f2f13a77462a42aafb3f52dd39c3e40278
Asukayanami <3184311912@qq.com>
2026-05-18 23:28:59 +0800

    软编码jwt密钥，使用配置文件隔离生产环境与开发环境

--- backend/src/main/java/com/asuka/backend/utils/JwtUtil.java
+++ backend/src/main/java/com/asuka/backend/utils/JwtUtil.java
@@ -1,20 +1,28 @@
 package com.asuka.backend.utils;
 
+import com.asuka.backend.properties.JwtProperties;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
 import java.util.Date;
 import java.util.Map;
 
+@Component
 public class JwtUtil {
-
-    private static final SecretKey SECRET_KEY = Keys.hmacShaKeyFor(
-            "AsukaBlog2026SecretKeyForJWTTokenGeneration!@#$%".getBytes()
-    );
+    private static SecretKey SECRET_KEY;
 
     private static final long EXPIRATION = 86400000L;
+
+    @Autowired
+    public void init(JwtProperties jwtProperties) {
+        SECRET_KEY = Keys.hmacShaKeyFor(
+                jwtProperties.getAdminSecretKey().getBytes()
+        );
+    }
 
     public static String createToken(Map<String, Object> claims) {
         return Jwts.builder()
